75c714b41eaccee3f9f04ae55c5022e7a6aabb29
Ivan-Slavkovic <ivanslavkovic.is@gmail.com>
2021-12-29 15:06:05 +0100

    Array difference

--- findOutlier.js
+++ findOutlier.js
@@ -12,30 +12,13 @@ Should return: 11 (the only odd number)
 Should return: 160 (the only even number)
 */
 
-// function findOutlier(integers) {
-// 	let array = [];
-// 	for (let i = 0; i < integers.length; i++) {
-// 		if (integers[i] % 2 === 0) {
-// 			array.push('even');
-// 		} else {
-// 			array.push('odd');
-// 		}
-// 	}
-// 	let temp = integers[0] % 2;
-// 	if (!temp === 0) {
-// 		for (let i = 1; i < integers.length; i++) {}
-// 	}
-// }
-
 function findOutlier(integers) {
 	function par(i) {
 		return i & 1;
 	}
-
 	let parity = par(integers[0]);
 	if (parity != par(integers[1])) {
 		if (parity == par(integers[2])) return integers[1];
-
 		return integers[0];
 	}
 	return integers.find((i) => par(i) != parity);
